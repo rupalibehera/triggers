37636ddefe4de8c1811ec02073d91181f1cd24ea
Rupali Behera <rbehera@redhat.com>
2021-12-01 15:59:54 +0100

    Some more improvements with update-to-head.sh which includes 1. removing the release.yaml as it is no more required. 2. disabling the push to release-next-ci branch as we do trigger any CI after pushing 3. Disabling the PR creation as it is of no use

--- openshift/release/update-to-head.sh
+++ openshift/release/update-to-head.sh
@@ -28,35 +28,4 @@ if [[ -d openshift/patches ]];then
     done
 fi
 
-# add release.yaml from previous successful nightly build to re-synced release-next as a backup
-git fetch ${OPENSHIFT_REMOTE} release-next
-
-git checkout FETCH_HEAD openshift/release/tektoncd-triggers-nightly.yaml
-
-git add openshift/release/tektoncd-triggers-nightly.yaml
-git commit -m ":robot: Add previous days release.yaml as back up"
-
 git push -f ${OPENSHIFT_REMOTE} release-next
-
-# Trigger CI
-git checkout release-next -B release-next-ci
-
-./openshift/release/generate-release.sh nightly
-
-date > ci
-git add ci openshift/release/tektoncd-triggers-nightly.yaml
-git commit -m ":robot: Triggering CI on branch 'release-next' after synching to upstream/master"
-
-git push -f ${OPENSHIFT_REMOTE} release-next-ci
-
-# removing upstream remote so that hub points origin for hub pr list command due to this issue https://github.com/github/hub/issues/1973
-git remote remove upstream
-already_open_github_issue_id=$(hub pr list -s open -f "%I %l%n"|grep ${LABEL}| awk '{print $1}'|head -1)
-[[ -n ${already_open_github_issue_id} ]]  && {
-    echo "PR for nightly is already open on #${already_open_github_issue_id}"
-    #hub api repos/${OPENSHIFT_ORG}/${REPO_NAME}/issues/${already_open_github_issue_id}/comments -f body='/retest'
-    exit 0
-}
-
-hub pull-request -m "🛑🔥 Triggering Nightly CI for ${REPO_NAME} 🔥🛑" -m "/hold" -m "Nightly CI do not merge :stop_sign:" \
-    --no-edit -l "${LABEL}" -b ${OPENSHIFT_REMOTE}/${REPO_NAME}:release-next -h ${OPENSHIFT_REMOTE}/${REPO_NAME}:release-next-ci
